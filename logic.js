e70bd6781df12d709f1887236168c5b484bce6ef
Lytkini <ilytkin88@gmail.com>
2020-06-15 23:18:17 +0300

    first prototype

--- logic.js
+++ logic.js
@@ -1,5 +1,21 @@
 const container = document.querySelector('#container');
 
-//container.append('Hello World!');
-container.textContent = 'Hello World!';
-console.log('test');
+let orderNumber = 1;
+
+//container.textContent = 'Hello World!';
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
+    // div.textContent = orderNumber;
+    div.setAttribute('id', 'childDiv')
+    container.appendChild(div);
+    orderNumber++;
+}
+
+const childDivs = document.querySelectorAll('#childDiv');
+
+childDivs.forEach(childDiv => {
+    childDiv.addEventListener('mouseover', function(e) {
+        e.target.style.background = 'black';
+    }); 
+});
