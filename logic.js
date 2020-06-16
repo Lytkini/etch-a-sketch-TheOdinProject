7f8e6cef7e4be0095dbff1ebad6db3252632e7b6
Lytkin <i.lytkin@evotor.ru>
2020-06-16 16:26:10 +0300

    added start up prompt, reset button, centered field

--- logic.js
+++ logic.js
@@ -1,21 +1,45 @@
+//aska user for a square size at start
+let squareSize = prompt('Enter square size (up to 64):', 10);
+
+
 const container = document.querySelector('#container');
+
+
+const controls = document.querySelector('#controls');
+const reset = controls.querySelector('#reset');
+
+let childDivWidth = 100/squareSize;
+
+
+reset.addEventListener('click', () => {
+    squareSize = prompt('Enter square size (up to 64):');
+    childDivs.forEach(childDiv => {
+        childDiv.style.background = 'lightblue';
+    });
+    console.log('' + squareSize);
+});
+
+console.log(squareSize);
 
 let orderNumber = 1;
 
-//container.textContent = 'Hello World!';
-
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < squareSize*squareSize; i++) {
     let div = document.createElement('div');
-    // div.textContent = orderNumber;
-    div.setAttribute('id', 'childDiv')
     container.appendChild(div);
     orderNumber++;
 }
 
-const childDivs = document.querySelectorAll('#childDiv');
+const childDivs = container.querySelectorAll('div');
+
+childDivs.forEach(childDiv => {
+    childDiv.style.width = childDivWidth + '%';
+    childDiv.style.height = childDivWidth + '%';
+    childDiv.style.flexBasis = childDivWidth + '%';
+});
 
 childDivs.forEach(childDiv => {
     childDiv.addEventListener('mouseover', function(e) {
         e.target.style.background = 'black';
-    }); 
+    });
 });
+
