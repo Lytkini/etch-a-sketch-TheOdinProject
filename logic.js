f3e4d9c0c95a4927286f60c5741cb21485b93808
Lytkin <i.lytkin@evotor.ru>
2020-06-16 17:42:05 +0300

    abstracted logic into a function, css cleaned up, color randomizer added

--- logic.js
+++ logic.js
@@ -1,45 +1,54 @@
-//aska user for a square size at start
-let squareSize = prompt('Enter square size (up to 64):', 10);
-
-
 const container = document.querySelector('#container');
-
-
 const controls = document.querySelector('#controls');
 const reset = controls.querySelector('#reset');
 
-let childDivWidth = 100/squareSize;
+let squareSize = prompt('Enter square size:', 10);
 
+let childDivWidth = 0;
+let childDivs = container.querySelectorAll('div');
+let color = '';
 
-reset.addEventListener('click', () => {
-    squareSize = prompt('Enter square size (up to 64):');
-    childDivs.forEach(childDiv => {
-        childDiv.style.background = 'lightblue';
-    });
-    console.log('' + squareSize);
-});
-
-console.log(squareSize);
-
-let orderNumber = 1;
-
-for (let i = 0; i < squareSize*squareSize; i++) {
-    let div = document.createElement('div');
-    container.appendChild(div);
-    orderNumber++;
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
-const childDivs = container.querySelectorAll('div');
+function createEtchASketchField(squareSize) {
+    container.style.borderBottom = '0px';
+    container.style.borderRight = '0px';
 
-childDivs.forEach(childDiv => {
-    childDiv.style.width = childDivWidth + '%';
-    childDiv.style.height = childDivWidth + '%';
-    childDiv.style.flexBasis = childDivWidth + '%';
-});
+    childDivWidth = 100 / squareSize;
 
-childDivs.forEach(childDiv => {
-    childDiv.addEventListener('mouseover', function(e) {
-        e.target.style.background = 'black';
+    for (let i = 0; i < squareSize * squareSize; i++) {
+        let orderNumber = 1;
+        let div = document.createElement('div');
+        container.appendChild(div);
+        orderNumber++;
+    };
+
+    childDivs = container.querySelectorAll('div');
+
+    childDivs.forEach(childDiv => {
+        childDiv.style.width = childDivWidth + '%';
+        childDiv.style.height = childDivWidth + '%';
+        childDiv.style.flexBasis = childDivWidth + '%';
+        childDiv.addEventListener('mouseover', function (e) {
+            //color = getRandomColor();
+            e.target.style.background = 'black';
+        });
     });
-});
+};
 
+createEtchASketchField(squareSize);
+
+reset.addEventListener('click', () => {
+    childDivs.forEach(childDiv => {
+        childDiv.remove();
+    });
+    squareSize = prompt('Enter square size (up to 64):');
+    createEtchASketchField(squareSize);
+});
