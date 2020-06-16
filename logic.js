74cbd7b1dea0fe9f254158f5999a3e1ba6cd77de
Lytkini <ilytkin88@gmail.com>
2020-06-16 21:18:26 +0300

    gradient added, css cleaned up

--- logic.js
+++ logic.js
@@ -1,12 +1,11 @@
 const container = document.querySelector('#container');
 const controls = document.querySelector('#controls');
+const start = controls.querySelector('#start');
 const reset = controls.querySelector('#reset');
 
-let squareSize = prompt('Enter square size:', 10);
-
-let childDivWidth = 0;
+let squareSize = 0;
 let childDivs = container.querySelectorAll('div');
-let color = '';
+console.log(childDivs);
 
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
@@ -18,10 +17,14 @@ function getRandomColor() {
 }
 
 function createEtchASketchField(squareSize) {
-    container.style.borderBottom = '0px';
-    container.style.borderRight = '0px';
+    let penColor = prompt('Chose pen color (black, random or gradient):');
+    let childDivWidth = 100 / squareSize;
+    let color = '';
 
-    childDivWidth = 100 / squareSize;
+    if (squareSize > 0) {
+        container.style.borderTop = '1px solid black';
+        container.style.borderLeft = '1px solid black';
+    }
 
     for (let i = 0; i < squareSize * squareSize; i++) {
         let orderNumber = 1;
@@ -36,16 +39,54 @@ function createEtchASketchField(squareSize) {
         childDiv.style.width = childDivWidth + '%';
         childDiv.style.height = childDivWidth + '%';
         childDiv.style.flexBasis = childDivWidth + '%';
-        childDiv.addEventListener('mouseover', function (e) {
-            //color = getRandomColor();
-            e.target.style.background = 'black';
-        });
     });
+
+    if (penColor === 'black' || penColor === '') {
+        childDivs.forEach(childDiv => {
+            childDiv.addEventListener('mouseover', function (e) {
+                e.target.style.background = 'black';
+            });
+        });
+    } else if (penColor === 'random') {
+        childDivs.forEach(childDiv => {
+            childDiv.addEventListener('mouseover', function (e) {
+                color = getRandomColor();
+                e.target.style.background = color;
+            });
+        });
+    } else if (penColor === 'gradient') {
+        let currentOpacity = 0;
+        childDivs.forEach(childDiv => {
+            childDiv.addEventListener('mouseover', function (e) {
+                currentOpacity = Number(e.target.style.opacity);
+                console.log('current opacity' + currentOpacity);
+                if (currentOpacity < 1) {
+                    currentOpacity += 0.1;
+                }
+                e.target.style.background = 'black';
+                e.target.style.opacity = currentOpacity;
+            });
+        });
+    };
 };
 
-createEtchASketchField(squareSize);
+start.addEventListener('click', () => {
+    if (childDivs.length != 0) {
+        alert('Press Reset to reset field.');
+        return;
+    }
+    squareSize = prompt('Enter square size (up to 64):');
+    createEtchASketchField(squareSize);
+    console.log(childDivs);
+});
 
 reset.addEventListener('click', () => {
+    if (childDivs.length === 0) {
+        alert('There is nothing to reset. Press Start');
+        return;
+    }
+    container.style.borderTop = '';
+    container.style.borderLeft = '';
     childDivs.forEach(childDiv => {
         childDiv.remove();
     });
